bbb4b4e160b260f46468239b83f6a7b1813b50bd
Prateek Singhal <psinghal@salesforce.com>
2022-05-17 16:50:48 -0700

    chore: build changes

--- serviceB/src/test/java/com/demo/serviceb/ApplicationTests.java
+++ serviceB/src/test/java/com/demo/serviceb/ApplicationTests.java
@@ -1,13 +1,7 @@
 package com.demo.serviceb;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
+public class ApplicationTests {
 }
